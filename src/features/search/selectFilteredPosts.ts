0832f0b98dfc0892e7f76577d113a979b957c60e
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-04-27 22:14:16 +0200

    refactor(search): make category optional and normalize case in filter

--- src/features/search/selectFilteredPosts.ts
+++ src/features/search/selectFilteredPosts.ts
@@ -2,14 +2,14 @@ import type { Post } from '../posts/postsSlice';
 
 export function selectFilteredPosts(state: {
   posts: { posts: Post[] };
-  filter: { query: string, category: string };
+  filter: { query: string, category?: string };
 }) {
   return state.posts.posts.filter((post) => {
     const title = post.title.toLowerCase();
     const query = state.filter.query.toLowerCase();
     const category = state.filter.category?.toLowerCase();
 
-    return (!category || post.subreddit === category) && title.includes(query);
+    return (!category || post.subreddit.toLowerCase() === category) && title.includes(query);
 
 
   });
